892fe76041732f32b00a296998b1cbc62f046ed3
Deyber Manrique <dbrmanrique@gmail.com>
2024-07-19 15:17:53 -0500

    Modificando los modelos para los ejes tematicos

--- app/Models/EjeTematico.php
+++ app/Models/EjeTematico.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class EjeTematico extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'eje_tematicos';
+
+    protected $fillable = [
+        'nombre',
+    ];
+
+    public function ponente_ponencias()
+    {
+        return $this->hasMany(PonentePonencia::class);
+    }
 }
--- app/Models/PonentePonencia.php
+++ app/Models/PonentePonencia.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class PonentePonencia extends Model
 {
-    use HasFactory;
+    use HasFactory,SoftDeletes;
+
+    protected $table = 'ponentes_ponencia';
+
+    protected $fillable = [
+        'estado',
+        'uuid',
+        'ponente_id',
+        'ponencia_id',
+        'eje_tematico_id',
+    ];
+
+    public function eje_tematico()
+    {
+        return $this->belongsTo(EjeTematico::class);
+    }
 }
