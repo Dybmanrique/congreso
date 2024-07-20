62d135a67bf85dc34b418055068a7d686eb3dd76
Deyber Manrique <dbrmanrique@gmail.com>
2024-07-19 22:26:55 -0500

    Cambiando el texto del dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -3,11 +3,11 @@
 @section('title', 'Dashboard')
 
 @section('content_header')
-    <h1>Dashboard</h1>
+    <h1>Panel de administración</h1>
 @stop
 
 @section('content')
-    <p>Welcome to this beautiful admin panel.</p>
+    <p>Bienvenido al panel de administración.</p>
 @stop
 
 @section('css')
