ecd80da9aa005ecfa4856e3af90ae326c40653f1
Deyber Manrique <dbrmanrique@gmail.com>
2024-07-20 13:19:54 -0500

    Cambiando el icono

--- resources/views/admin/participantes/index.blade.php
+++ resources/views/admin/participantes/index.blade.php
@@ -127,7 +127,7 @@
                 "data": null,
                 "render": function(data, type, row, meta) {
                     template =
-                        `<button class="btn btn-sm btn-info mr-2" onclick="ver('${data.imagen_comprobante}')" type="button" data-toggle="modal" data-target="#modalVer"><i class=" fas fa-eye"></i> Ver comprobante</button>`;
+                        `<button class="btn btn-sm btn-info mr-2" onclick="ver('${data.imagen_comprobante}')" type="button" data-toggle="modal" data-target="#modalVer"><i class="far fa-file-alt"></i> Ver comprobante</button>`;
                     if (data.es_valido == 0) {
                         template +=
                             `<button class="btn btn-sm btn-primary mr-2" onclick="validar(${data.id})" type="button"><i class=" fas fa-check"></i> Validar</button>`;
