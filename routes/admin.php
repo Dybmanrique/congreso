f2b994c409a2384d466f9b5797a4073ee8d8ba75
Deyber Manrique <dbrmanrique@gmail.com>
2024-07-20 01:15:08 -0500

    agregando el middleware de sanctum

--- routes/admin.php
+++ routes/admin.php
@@ -3,5 +3,13 @@
 use App\Http\Controllers\Admin\ParticipantesController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/participantes', [ParticipantesController::class, 'index'])->name('admin.participantes.index');
-Route::get('/participantes/data', [ParticipantesController::class, 'data'])->name('admin.participantes.data');
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('participantes', [ParticipantesController::class, 'index'])->name('admin.participantes.index');
+    Route::get('participantes/data', [ParticipantesController::class, 'data'])->name('admin.participantes.data');
+    Route::post('participantes/validar', [ParticipantesController::class, 'validar'])->name('admin.participantes.validar');
+});
