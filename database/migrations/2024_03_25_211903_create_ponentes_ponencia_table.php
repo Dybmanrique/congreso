85017a064b18f3265547c594116e9d87acb3c895
Deyber Manrique <dbrmanrique@gmail.com>
2024-07-20 20:51:42 -0500

    Agregando el atributo de es valido a los ponentes ponencia

--- database/migrations/2024_03_25_211903_create_ponentes_ponencia_table.php
+++ database/migrations/2024_03_25_211903_create_ponentes_ponencia_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->foreignId('ponencia_id')->constrained('ponencias')->onDelete('cascade')->onUpdate('cascade');
             $table->foreignId('eje_tematico_id')->constrained('eje_tematicos')->onDelete('cascade')->onUpdate('cascade');
             $table->boolean('estado')->default(0);
+            $table->boolean('es_valido')->default(0);
             $table->char('uuid', length: 36)->nullable();
             $table->timestamps();
             $table->softDeletes();
